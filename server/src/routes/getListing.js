e3646dc6f2f5edd0364387352c8c290761e6905b
Troy Dorman <troydorman@gmail.com>
2021-04-15 15:47:52 -0600

    installed hapi plugin boom to handle errors.  implemented its use on our get listing route

--- server/src/routes/getListing.js
+++ server/src/routes/getListing.js
@@ -1,10 +1,15 @@
 import { fakeListings } from './fake-data';
+import Boom from '@hapi/boom';
 
 export const getListingRoute = {
     method: 'GET',
     path: '/api/listings/{id}',
     handler: (req, h) => {
         const id = req.params.id;
-        return fakeListings.find(listing => listing.id === id);
+        const listing = fakeListings.find(listing => listing.id === id);
+        if (!listing) throw Boom.notFound(`Listing does not exist with id ${id}`);
+        return listing;
+        
+
     }
 }
