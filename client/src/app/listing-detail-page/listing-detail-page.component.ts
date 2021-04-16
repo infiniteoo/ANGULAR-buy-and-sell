4a603ee7c60a3c5d7fca50ba4e8ab8b1a8823f04
Troy Dorman <troydorman@gmail.com>
2021-04-16 09:43:57 -0600

    connect the listing detail page and implemented view counter in the html

--- client/src/app/listing-detail-page/listing-detail-page.component.ts
+++ client/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { fakeListings } from '../fake-data';
 import { Listing } from '../types';
+import { ListingsService } from '../listings.service';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -9,15 +10,23 @@ import { Listing } from '../types';
   styleUrls: ['./listing-detail-page.component.css']
 })
 export class ListingDetailPageComponent implements OnInit {
+  isLoading: boolean = true;
   listing: Listing;
 
   constructor(
     private route: ActivatedRoute,
+    private listingsService: ListingsService
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find(listing => listing.id === id);
+    this.listingsService.getListingById(id)
+    .subscribe(listing => {
+      this.listing = listing;
+      this.isLoading = false;
+    });
+    this.listingsService.addViewToListing(id)
+    .subscribe(() => console.log('Views updated!'));
   }
 
 }
