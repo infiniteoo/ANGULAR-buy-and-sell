2f2e2b62d22c1357e599b7a1c7b06595b4030610
Troy Dorman <troydorman@gmail.com>
2021-04-16 12:58:59 -0600

    added sign in with google ability.  so cool!

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import firebase  from 'firebase/app';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'buy-and-sell';
+
+  constructor(
+    public auth: AngularFireAuth,
+  ) { }
+
+  signInClicked(): void {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  signOutClicked(): void {
+    this.auth.signOut();
+  }
+
+
 }
