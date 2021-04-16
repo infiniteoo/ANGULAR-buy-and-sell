607115324ff2b90ebc780dc913f5a8570ac4ecbf
Troy Dorman <troydorman@gmail.com>
2021-04-16 08:14:51 -0600

    created the get user listings route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 import { getAllListingsRoute } from './getAllListings';
 import { getListingRoute } from './getListing';
 import { addViewToListingRoute } from './addViewToListing';
+import { getUserListingsRoute } from './getUserListings';
 
 export default [
     getAllListingsRoute,
     getListingRoute,
     addViewToListingRoute,
+    getUserListingsRoute,
 ]
